8bb60435b4cb062798eeee1b923b549567b1fe48
MTito2 <mateuscunha166@gmail.com>
2026-04-15 12:12:47 -0300

    add func add and more quantity

--- Frontend/js/pages/productsPage.js
+++ Frontend/js/pages/productsPage.js
@@ -1,6 +1,7 @@
 import { getProducts } from '../api/productApi.js';
 const productsContainer = document.getElementById('products-container');
 
+// Formatar o preço para o formato brasileiro (R$ 10,00)
 let response = await getProducts();
 let products = response;
 const formattedProducts = products.map(product => {
@@ -10,11 +11,23 @@ const formattedProducts = products.map(product => {
     };
 });
 
+// Criar os cards dos produtos e adicioná-los ao container
 for (let product of formattedProducts) {
     const productCard = createProductCard(product);
     productsContainer.appendChild(productCard);
 }
 
+const btnProducts = document.querySelectorAll('.btn-product'); 
+
+
+for (let btn of btnProducts) {
+    btn.addEventListener('click', (e) => { 
+        const action = e.target.dataset.action;
+        const qtdElement = e.target.parentElement.querySelector('.qtd');
+        qtdElement.textContent = action === 'add' ? parseInt(qtdElement.textContent) + 1 : Math.max(0, parseInt(qtdElement.textContent) - 1);
+    })};
+
+// Função para criar o card do produto
 function createProductCard(product) {
     const card = document.createElement('div');
     card.className = 'border-0 card mb-3 w-100';
@@ -33,12 +46,13 @@ function createProductCard(product) {
             </div>
             
             <div class="col-3 d-flex align-items-start justify-content-around gap-3">
-                <img src="../assets/icons/less.svg" alt="">
-                <p class="text-dark">0</p>
-                <img src="../assets/icons/add.svg" alt="">
+                <img class="btn-product" data-action="sub" src="../assets/icons/less.svg" alt="">
+                <p class="qtd text-dark">0</p>
+                <img class="btn-product" data-action="add" src="../assets/icons/add.svg" alt="">
             </div>
         </div>
     `;
 
     return card;
 }
+
