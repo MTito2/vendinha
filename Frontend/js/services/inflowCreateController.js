52e450a74f6484d9e2ec063095aa1b04c3b91bfd
Mateus da Cunha Tito <134788822+MTito2@users.noreply.github.com>
2026-05-01 10:16:57 -0300

    feat(css): add focus styles for contenteditable elements to enhance user experience feat(inflowCreateController.js): implement button listener for adding new product rows in the inflow table refactor(inflows.html, outflows.html, products_adm.html): remove redundant table-hover class for consistency in table styling fix(inflows.css): update button ID from btn-new-row to btn-new-product for clarity and functionality

--- Frontend/js/services/inflowCreateController.js
+++ Frontend/js/services/inflowCreateController.js
@@ -9,7 +9,28 @@ export class InflowCreateView {
     }
 
     renderTable() {
+        this.btnNewProductListener()
         
     }
 
+    btnNewProductListener () {
+        const btnNewProduct = document.getElementById("btn-new-product")
+        const tableBody = document.querySelector("tbody")
+
+        btnNewProduct.addEventListener("click", () => {
+            const row = document.createElement("tr")
+
+            for (let index = 0; index < 4; index++) {
+                const cell = document.createElement("td")
+                cell.classList.add("table-cell", "align-middle", "text-center")
+                cell.setAttribute("contenteditable", "true")
+                row.appendChild(cell)
+            }
+
+            tableBody.append(row)
+
+            const firstCell = row.firstElementChild
+            firstCell.focus()
+        })
+    }
 }
