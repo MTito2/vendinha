e88ad4ceb53eba32df7a40abc3ad6a983a6c834a
MTito2 <mateuscunha166@gmail.com>
2026-05-12 15:33:16 -0300

    fix(PlaceModel.cs): change Id property from init-only to settable to allow modification after object creation

--- Models/PlaceModel.cs
+++ Models/PlaceModel.cs
@@ -2,7 +2,7 @@
 {
     public class PlaceModel
     {
-        public int Id { get; init; }
+        public int Id { get; set; }
         public string Name { get; set; }
 
         public PlaceModel() { }
